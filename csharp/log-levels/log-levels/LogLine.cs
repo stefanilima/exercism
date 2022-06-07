9d6215f11c6556e426e414c3c8dda6b0faf38814
stefanilima <stefanisilvadelima@hotmail.com>
2022-06-06 23:35:42 -0300

    Exercicios log-level

---
+++ csharp/log-levels/log-levels/LogLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace log_levels
+{
+    static class LogLine
+    {
+        public static string Message(string logLine)
+        {
+            char[] remove = {' ', '\r', '\n', '\t'};
+            string[] str = logLine.Split(':');
+
+            return str[1].TrimEnd(remove).TrimStart(remove);
+        }
+
+        public static string LogLevel(string logLine)
+        {
+            char[] remove = {'['};
+            string[] str = logLine.Split(']');
+
+            return str[0].ToLower().TrimStart(remove);
+        }
+
+        public static string Reformat(string logLine)
+        {
+            string message = Message(logLine);
+            string level = LogLevel(logLine);
+
+
+            return $"{message} ({level})";
+        }
+    }
+
+}
---
+++ csharp/log-levels/log-levels/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace log_levels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(LogLine.Message("[ERROR]: Invalid operation"));
+            Console.WriteLine(LogLine.Message("[INFO]: Timezone changed"));
+            Console.WriteLine(LogLine.Message("[WARNING]: Disk almost full"));
+            Console.WriteLine(LogLine.Message("[WARNING]:  Disk almost full\r\n"));
+
+            Console.WriteLine(LogLine.LogLevel("[ERROR]: Invalid operation"));
+            Console.WriteLine(LogLine.LogLevel("[WARNING]: Unsafe password"));
+            Console.WriteLine(LogLine.LogLevel("[INFO]: Timezone changed"));
+
+            Console.WriteLine(LogLine.Reformat("[ERROR]: Segmentation fault"));
+            Console.WriteLine(LogLine.Reformat("[WARNING]: Decreased performance"));
+            Console.WriteLine(LogLine.Reformat("[INFO]: Operation completed"));
+            Console.WriteLine(LogLine.Reformat("[ERROR]: \t Corrupt disk\t \t \r\n"));
+
+        }
+    }
+}

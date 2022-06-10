7f5c10e6ed6c1fd8a8d04ba40fd83f9277443c90
stefanilima <stefanisilvadelima@hotmail.com>
2022-06-09 23:56:08 -0300

    exercicio cars-assemble

---
+++ csharp/cars-assemble/AssemblyLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (speed == 0) return 0;
+        else if (speed >= 1 && speed <= 4) return 1;
+        else if (speed >= 5 && speed <= 8) return 0.9;
+        else if (speed == 9) return 0.8;
+        else if (speed == 10) return 0.77;
+
+        return 0;
+    }
+
+    public static double ProductionRatePerHour(int speed)
+    {
+        var total = (double)(speed * 221);
+        var success = SuccessRate(speed);
+
+        return total * success;
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        return (int)(ProductionRatePerHour(speed) / 60);
+    }
+}
---
+++ csharp/cars-assemble/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cars_assemble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(AssemblyLine.SuccessRate(0));
+            Console.WriteLine(AssemblyLine.SuccessRate(1));
+            Console.WriteLine(AssemblyLine.SuccessRate(4));
+            Console.WriteLine(AssemblyLine.SuccessRate(9));
+            Console.WriteLine(AssemblyLine.SuccessRate(10));
+
+            Console.WriteLine(AssemblyLine.ProductionRatePerHour(0));
+            Console.WriteLine(AssemblyLine.ProductionRatePerHour(1));
+            Console.WriteLine(AssemblyLine.ProductionRatePerHour(4));
+            Console.WriteLine(AssemblyLine.ProductionRatePerHour(7));
+
+            Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(0));
+            Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(1));
+            Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(5));
+            Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(8));
+            Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(9));
+            Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(10));
+        }
+    }
+}

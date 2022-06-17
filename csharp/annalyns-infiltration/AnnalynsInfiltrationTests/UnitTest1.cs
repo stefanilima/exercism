07984dcd4088c8fb5f378ce7c865d92048410bad
stefanilima <stefanisilvadelima@hotmail.com>
2022-06-17 16:17:41 -0300

    Ajustado solução para o exercicio annalyns-infiltration

---
+++ csharp/annalyns-infiltration/AnnalynsInfiltrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace AnnalynsInfiltrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ csharp/elons-toys/ElonsToysTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace ElonsToysTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

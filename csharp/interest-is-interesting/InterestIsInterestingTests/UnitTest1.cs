8430ca27424f427e32075826c8fef0417d6b5ed7
stefanilima <stefanisilvadelima@hotmail.com>
2022-06-22 15:02:32 -0300

    Exercicio interest-is-interesting

---
+++ csharp/interest-is-interesting/InterestIsInterestingTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace InterestIsInterestingTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

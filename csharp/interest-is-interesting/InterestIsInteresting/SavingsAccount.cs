70dbdd3a75450fce9f0950f15c939b5581c5c058
stefanilima <stefanisilvadelima@hotmail.com>
2022-06-22 22:53:44 -0300

    Exercicio interest-is-interesting

---
+++ csharp/interest-is-interesting/InterestIsInteresting/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace InterestIsInteresting
+{
+    public static class SavingsAccount
+    {
+        public static float InterestRate(decimal balance)
+        {
+            if (balance < 0) return 3.213f;
+            else if (balance < 1000) return 0.5f;
+            else if (balance >= 1000 && balance < 5000) return 1.621f;
+            else return 2.475f;
+        }
+
+        public static decimal Interest(decimal balance)
+        {
+            throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
+        }
+
+        public static decimal AnnualBalanceUpdate(decimal balance)
+        {
+            throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+        }
+
+        public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+        {
+            throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        }
+    }
+}
---
+++ csharp/interest-is-interesting/InterestIsInterestingTests/SavingsAccountTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using InterestIsInteresting;
+using Xunit;
+
+namespace InterestIsInterestingTests
+{
+    public class SavingsAccountTest
+    {
+        [Fact]
+        public void InterestRateWithBalanceNegativo()
+        {
+            Assert.Equal(3.213f, SavingsAccount.InterestRate(-1));
+        }
+
+        [Fact]
+        public void InterestRateWithLessBalanceThousand()
+        {
+            Assert.Equal(0.5f, SavingsAccount.InterestRate(999));
+        }
+
+        [Fact]
+        public void InterestRateWithBalanceEqualToThousand()
+        {
+            Assert.Equal(1.621f, SavingsAccount.InterestRate(1000));
+        }
+
+        [Fact]
+        public void InterestRateWithBalanceGreaterLessThanFiveThousand()
+        {
+            Assert.Equal(1.621f, SavingsAccount.InterestRate(4001));
+        }
+
+        [Fact]
+        public void InterestRateWithBalanceGreaterThanFiveThousand()
+        {
+            Assert.Equal(2.475f, SavingsAccount.InterestRate(5001));
+        }
+    }
+}
